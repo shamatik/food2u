9ea249c7e845c57241b7ce0953396d1c1b3928c1
Edgar Burelo <edgar.burelo@gmail.com>
2018-09-24 17:05:17 -0500

    add Fireb

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -22,6 +22,17 @@ $.ajax({
 //fin de ejemplo
 
 //objeto General 
+var config = {
+    apiKey: "AIzaSyB-rdpy21o-_3Sg2p1kzHD9MzFsgxdin7U",
+    authDomain: "food2u-dfedd.firebaseapp.com",
+    databaseURL: "https://food2u-dfedd.firebaseio.com",
+    projectId: "food2u-dfedd",
+    storageBucket: "food2u-dfedd.appspot.com",
+    messagingSenderId: "137763401473"
+};
+
+firebase.initializeApp(config);
+var dataB = firebase.database();
 
 var food2U = {
     //Propiedades y métodos del objeto definir las funciones(métodos), dentro del objeto.
